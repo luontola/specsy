071ae166a6c18418d8a96058fe171004c828438d
Esko Luontola <esko.luontola@gmail.com>
2010-07-20 17:54:41 +0300

    More tests for ResultCollector

--- src/test/scala/net/orfjackal/specsy/runner/ResultCollectorTest.scala
+++ src/test/scala/net/orfjackal/specsy/runner/ResultCollectorTest.scala
@@ -8,7 +8,7 @@ import net.orfjackal.specsy.Specsy
 import net.orfjackal.specsy.runner.notification.TestSuiteNotifier
 
 class ResultCollectorTest {
-  private val collector = new ResultCollector()
+  private val collector = new ResultCollector
   private val suite = collector: TestSuiteNotifier
 
   @Test
@@ -104,11 +104,101 @@ class ResultCollectorTest {
       )))
   }
 
-  // TODO: multiple test classes
-  // TODO: nested specs
-  // TODO: concurrent execution order
+  @Test
+  def test_class_with_nested_children() {
+    suite.fireSuiteStarted()
+    val clazz = suite.newTestClassNotifier(classOf[DummySpec])
+    clazz.fireTestClassStarted()
+
+    val run = clazz.newTestRunNotifier()
+    run.fireTestStarted(Path(), "root")
+    run.fireTestStarted(Path(0), "child A")
+    run.fireTestStarted(Path(0, 0), "child AA")
+    run.fireTestFinished(Path(0, 0))
+    run.fireTestFinished(Path(0))
+    run.fireTestFinished(Path())
+
+    clazz.fireTestClassFinished()
+    suite.fireSuiteFinished()
+
+    assertThat(collector.visualizedTree, is(List(
+      classOf[DummySpec].getName,
+      "- root",
+      "  - child A",
+      "    - child AA"
+      )))
+  }
+
+  @Test
+  def multiple_test_classes() {
+    suite.fireSuiteStarted()
+
+    {
+      val clazz = suite.newTestClassNotifier(classOf[DummySpec])
+      clazz.fireTestClassStarted()
+
+      val run = clazz.newTestRunNotifier()
+      run.fireTestStarted(Path(), "root 1")
+      run.fireTestFinished(Path())
+
+      clazz.fireTestClassFinished()
+    }
+    {
+      val clazz = suite.newTestClassNotifier(classOf[DummySpec2])
+      clazz.fireTestClassStarted()
+
+      val run = clazz.newTestRunNotifier()
+      run.fireTestStarted(Path(), "root 2")
+      run.fireTestFinished(Path())
+
+      clazz.fireTestClassFinished()
+    }
+
+    suite.fireSuiteFinished()
+
+    assertThat(collector.visualizedTree, is(List(
+      classOf[DummySpec].getName,
+      "- root 1",
+      classOf[DummySpec2].getName,
+      "- root 2"
+      )))
+  }
+
+  @Test
+  def concurrently_executed_tests() { // test data based on test_class_with_many_children
+    suite.fireSuiteStarted()
+    val clazz = suite.newTestClassNotifier(classOf[DummySpec])
+    clazz.fireTestClassStarted()
+
+    val run1 = clazz.newTestRunNotifier()
+    run1.fireTestStarted(Path(), "root")
+    run1.fireTestStarted(Path(0), "child A")
+
+    val run2 = clazz.newTestRunNotifier()
+    run2.fireTestStarted(Path(), "root")
+    run2.fireTestStarted(Path(1), "child B")
+
+    run1.fireTestFinished(Path(0))
+    run1.fireTestFinished(Path())
+
+    run2.fireTestFinished(Path(1))
+    run2.fireTestFinished(Path())
+
+    clazz.fireTestClassFinished()
+    suite.fireSuiteFinished()
+
+    assertThat(collector.visualizedTree, is(List(
+      classOf[DummySpec].getName,
+      "- root",
+      "  - child A",
+      "  - child B"
+      )))
+  }
+
   // TODO: prevent calling the methods in the wrong order
 
   private class DummySpec extends Specsy {
+  }
+  private class DummySpec2 extends Specsy {
   }
 }
