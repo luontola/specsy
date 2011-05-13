ffda917987d1fd1517dfc25826979a9fbcf36a57
Esko Luontola <esko.luontola@gmail.com>
2011-05-14 01:21:21 +0300

    Added copyright headers to all files

--- src/main/scala/net/orfjackal/specsy/core/SpecDeclaration.scala
+++ src/main/scala/net/orfjackal/specsy/core/SpecDeclaration.scala
@@ -1,3 +1,7 @@
+// Copyright © 2010-2011, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
 package net.orfjackal.specsy.core
 
 class SpecDeclaration(
