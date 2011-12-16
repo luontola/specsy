a476e0ecde5a309a45865ebcce67b05aa5c98436
Esko Luontola <esko.luontola@gmail.com>
2011-12-16 07:20:10 +0200

    shBrushScala.js: do not highlight numbers which are part of an identifier

--- syntaxhighlighter/shBrushScala.js
+++ syntaxhighlighter/shBrushScala.js
@@ -35,7 +35,7 @@
 			{ regex: SyntaxHighlighter.regexLib.multiLineSingleQuotedString,	css: 'string' },	// multi-line strings
 			{ regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString,    css: 'string' },	// double-quoted string
 			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,				css: 'string' },	// strings
-			{ regex: /0x[a-f0-9]+|\d+(\.\d+)?/gi,								css: 'value' },		// numbers
+			{ regex: /\b0x[a-f0-9]+|\b\d+(\.\d+)?/gi,								css: 'value' },		// numbers
 			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),				css: 'keyword' },	// keywords
 			{ regex: new RegExp(keyops, 'gm'),									css: 'keyword' }	// scala keyword
 			];
