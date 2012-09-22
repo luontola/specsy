a23ab282ff72d1a7199e70a376f33c322cf6f3a4
Esko Luontola <esko.luontola@gmail.com>
2012-09-22 19:26:34 +0300

    Converted ContextDealer to Java

---
+++ specsy-core/src/main/java/org/specsy/core/ContextDealer.java
@@ -0,0 +1,23 @@
+// Copyright © 2010-2012, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package org.specsy.core;
+
+public class ContextDealer {
+
+    private static final ThreadLocal<Context> prepared = new ThreadLocal<>();
+
+    public static void prepare(Context context) {
+        prepared.set(context);
+    }
+
+    public static Context take() {
+        Context context = prepared.get();
+        if (context == null) {
+            throw new IllegalStateException("tried to take the context before it was prepared");
+        }
+        prepared.remove();
+        return context;
+    }
+}
